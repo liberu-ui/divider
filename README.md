f0b0639e4395ecd795660edd975c72cb9e31a86c
mishu <mishu@xtelecom.ro>
2019-03-05 07:40:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Divider
+
+[![License](https://poser.pugx.org/laravel-enso/divider/license)](https://packagist.org/packages/laravel-enso/divider)
+[![Latest Stable Version](https://poser.pugx.org/laravel-enso/divider/version)](https://packagist.org/packages/laravel-enso/divider)
+
+Divider
+
+### Features
+
+- soon
+
+### Configuration & Usage
+
+- soon
+
+### Contributions
+
+are welcome. Pull requests are great, but issues are good too.
+
+### License
+
+This package is released under the MIT license.
---
+++ src/Divider.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :class="['is-divider', placement]"
+        :data-title="title"/>
+</template>
+
+<script>
+export default {
+    name: 'Divider',
+
+    props: {
+        placement: {
+            type: String,
+            default: 'center',
+            validator: value => ['left', 'center', 'right'].includes(value),
+        },
+        title: {
+            type: String,
+            default: null,
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+    .is-divider {
+        height: 1px;
+        margin: .5rem 0;
+
+        &.left {
+            text-align: left;
+        }
+
+        &.center {
+            text-align: center;
+        }
+
+        &.right {
+            text-align: right;
+        }
+    }
+
+    .is-divider[data-title]::after {
+        content: attr(data-title);
+        display: inline-block;
+        font-size: 1rem;
+        -webkit-transform: translateY(-0.9rem);
+        -ms-transform: translateY(-0.9rem);
+        transform: translateY(-0.9rem);
+        padding: 0 .5rem;
+    }
+</style>
